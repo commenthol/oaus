3b49b52844a4586daa33428189a08a94a735f7da
commenthol <commenthol@users.noreply.github.com>
2018-06-01 18:15:49 +0200

    lower deadlines for manual tests

--- example/server/config.js
+++ example/server/config.js
@@ -1,2 +1,8 @@
 const config = require('../../test/config')
+
+Object.assign(config.oauth2, { // oauth2-server settings
+  accessTokenLifetime: 60, // in seconds
+  refreshTokenLifetime: 300
+})
+
 module.exports = config
